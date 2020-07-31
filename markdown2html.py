3b7e03ec14403d39eef963a5cecd4592a38df49d
Juan <1213@holbertonschool.com>
2020-07-31 19:48:57 +0000

    Start a script

--- markdown2html.py
+++ markdown2html.py
@@ -8,7 +8,7 @@ import sys
 import os.path as path
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 3:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         sys.exit(1)
 
