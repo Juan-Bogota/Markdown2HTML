542888833b54c1c92cf4bf11318953eda23008f0
Juan <1213@holbertonschool.com>
2020-07-31 22:20:06 +0000

    simple text

--- markdown2html.py
+++ markdown2html.py
@@ -16,36 +16,50 @@ if __name__ == "__main__":
         sys.stderr.write("Missing {}\n".format(sys.argv[1]))
         sys.exit(1)
     with open(sys.argv[1], "r") as readme:
-        text = ""
+        text, tmp = "", ""
+        text_ant = "\n"
         my_list = []
+        simbols = ["-", "*", " ", "#"]
         for line in readme.readlines():
-            if line != "\n":
-                count = 0
-                for caracter in line:
-                    if caracter != "-" and caracter != "*":
-                        my_list = []
-                    if caracter == "#":
-                        count += 1
-                        continue
-                    elif count:
-                        word = (line[count + 1:-1])
-                        text += "<h{}>{}</h{}>\n".format(count, word, count)
-                        tmp = text
+
+            count = 0
+            for caracter in line:
+                if caracter not in simbols:
+                    my_list = []
+                    if caracter == "\n":
+                        text_ant = caracter
                         break
-                    if caracter == "-" or caracter == "*":
-                        if my_list:
-                            text = tmp
-                        li_list = ""
-                        word = (line[2:-1])
-                        my_list.append(word)
-                        for li in my_list:
-                            li_list += "<li>{}</li>\n".format(li)
+                    if text_ant == "\n":
                         tmp = text
-                        if caracter == "-":
-                            text += "<ul>\n{}</ul>\n".format(li_list)
-                        else:
-                            text += "<ol>\n{}</ol>\n".format(li_list)
-                        break
+                        text += "<p>\n{}</p>\n".format(line)
+                    else:
+                        text = tmp
+                        text += "<p>\n{}<br>\n{}</p>\n".format(
+                            text_ant, line)
+                    text_ant = line
+                    break
+                if caracter == "#":
+                    count += 1
+                    continue
+                elif count:
+                    word = (line[count + 1:-1])
+                    text += "<h{}>{}</h{}>\n".format(count, word, count)
+                    tmp = text
+                    break
+                if caracter == "-" or caracter == "*":
+                    if my_list:
+                        text = tmp
+                    li_list = ""
+                    word = (line[2:-1])
+                    my_list.append(word)
+                    for li in my_list:
+                        li_list += "<li>{}</li>\n".format(li)
+                    tmp = text
+                    if caracter == "-":
+                        text += "<ul>\n{}</ul>\n".format(li_list)
+                    else:
+                        text += "<ol>\n{}</ol>\n".format(li_list)
+                    break
 
     with open(sys.argv[2], "w") as html:
         html.write(text)
